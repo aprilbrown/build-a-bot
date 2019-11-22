dd3c63eb908dbcaa23651e53af4058dfb1149459
April Brown <april.brown@clearlink.com>
2019-11-22 09:12:25 -0700

    finished routing section

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -75,6 +75,16 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
     name: 'RobotBuilder',
+    beforeRouteLeave(to, from, next) {
+        if (this.addedToCart) {
+            next(true);
+        } else {
+            /* eslint no-alert: 0 */
+            /* eslint no-restricted-globals: 0 */
+            const response = confirm('You have not added your robot to your cart, are you sure you want to leave?');
+            next(response);
+        }
+    },
     components: {
         PartSelector,
         CollapsibleSection,
@@ -82,6 +92,7 @@ export default {
     data() {
         return {
             avaliableParts,
+            addedToCart: false,
             cart: [],
             selectedRobot: {
                 head: {},
@@ -106,6 +117,7 @@ export default {
                 + robot.rightArm.cost
                 + robot.base.cost;
             this.cart.push(Object.assign({}, robot, { cost }));
+            this.addedToCart = true;
         },
     },
 };
