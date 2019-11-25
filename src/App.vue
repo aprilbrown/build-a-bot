d714e5b033af78d0a529ee2ee20614f4e395b9df
April Brown <april.brown@clearlink.com>
2019-11-25 08:32:04 -0700

    start of vue store

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,14 @@
                             Build
                         </router-link>
                     </li>
+                    <li class="nav-item cart">
+                        <router-link class="nav-link" :to="{name: 'Cart'}" exact>
+                            Cart
+                        </router-link>
+                        <div class="cart-items">
+                            {{cart.length}}
+                        </div>
+                    </li>
                 </ul>
             </nav>
         </header>
@@ -31,6 +39,11 @@
 <script>
 export default {
     name: 'app',
+    computed: {
+        cart() {
+            return this.$store.state.cart;
+        },
+    },
 };
 </script>
 
@@ -70,6 +83,12 @@ export default {
         border-right: 1px solid #bbb;
     }
 
+    .nav-item.cart {
+        position: relative;
+        margin-left: auto;
+        border-right: none;
+    }
+
     .logo {
         vertical-align: middle;
         height: 30px;
@@ -96,4 +115,17 @@ export default {
         width: 100px;
         min-height: 300px;
     }
+
+    .cart-items {
+        position: absolute;
+        top: -5px;
+        right: -9px;
+        font-size: 18px;
+        width: 20px;
+        text-align: center;
+        display: inline-block;
+        border-radius: 100px;
+        background-color: mediumseagreen;
+    }
+
 </style>
