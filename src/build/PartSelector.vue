ba61ccb211fd60c08be02aad2a77740917092626
April Brown <april.brown@clearlink.com>
2019-11-27 11:51:26 -0700

    finishing up module

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -3,7 +3,9 @@
         <img @click="showPartInfo" :src="selectedPart.src" title="arm"/>
         <button @click="selectPreviousPart()" class="prev-selector"></button>
         <button @click="selectNextPart()" class="next-selector"></button>
-        <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+        <span v-pin="{ bottom: '10px', right: '5px' }" class="sale" v-show="selectedPart.onSale">
+            Sale!
+        </span>
     </div>
 </template>
 
@@ -84,9 +86,6 @@ export default {
     }
 
     .sale {
-        position: absolute;
-        bottom: 5px;
-        right: 5px;
         color: white;
         background-color: red;
         padding: 3px;
